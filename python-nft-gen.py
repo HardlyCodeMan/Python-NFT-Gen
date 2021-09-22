d12daf4eb2650fb5a68f7e0dfc3c467d1d11cb00
HardlyCodeMan <graham@planbit.solutions>
2021-09-22 17:30:31 +0800

    Update python-nft-gen.py

--- python-nft-gen.py
+++ python-nft-gen.py
@@ -6,20 +6,85 @@ from PIL import Image
 # Main images dir
 baseDir = './images/'
 
-# File system, load images
-traitsPath = os.listdir(baseDir)
-print(traitsPath)
-traitsJSON = json.encoder(traitsPath)
-print(traitsJSON)
+#attributesFile  = open('./attributes.json',)
+attributesFile  = open('./data.json',)
+attributes = json.load(attributesFile)
 
-for root,directories,files in os.walk(baseDir, topdown=False):
-    for name in files:
-        print(os.path.join(root,name))
-    for name in directories:
-        print(os.path.join(root,name))
+attributesArray = []
+imagesArray = []
+weightsArray = []
+metadataArray = []
+
+i = 0
+j = 0 # image & weight
+# Load json into iteratable arrays
+#for (k,v) in attributes.items():
+#    tempArray = []
+#    while j < len(attributes[k]):
+#        tempArray = [attributes[k]]
+#        print(tempArray)
+#        j += 1
+#    i += 1
+#    attributesArray.append(tempArray)
+#
+#     print(attributesArray)
+
+j = 0
+my_json = attributes
+my_array = []
+tempArray = []
+#for key, value in my_json.items():
+#    print(value)
+#    while j < len(my_json[key]):
+#        tempArray.append(my_json[key][j])
+#        j += 1
+#    new_array = [key, tempArray]
+#    my_array.append(new_array)
+
+
+for key, value in my_json.items():
+    current_key = my_json[key]
+    new_array = [key]
+    for i in current_key:
+        new_array.append(i)
+        # for j in i:
+        #     new_array.append(j)
+    my_array.append(new_array)
+
+#print(my_array)
+print(my_array[0])
+print(my_array[0][0])
+print(my_array[0][0][0])
+
+#print(attributesArray)
+
+i = 0 # attribute name
+j = 0 # image & weight
+
+#while i < len(attributesArray):
+#    trait = str(attributesArray[i])
+#    print(trait)
+#    while j < len(attributes[trait]):
+#        image = attributes[trait][j][0]
+#        weight = attributes[trait][j][1]
+#        print(image)
+#       print(weight)
+#       j += 1
+#    i += 1
+
+#print(attributes['Fur'][0][0])
+#print(attributesArray[0])
+#print(imagesArray)
+
+def getImageName(data):
+    return data[0]
+
+def getImageWeight(data):
+    return data[1]
+
+#print(getImageName(imagesArray[1][1][0]))
 
 exit()
-
 # Load layer files
 background = './images/Aqua.png'
 fur = './images/Brown.png'
